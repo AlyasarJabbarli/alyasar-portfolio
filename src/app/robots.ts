9eb1a3294c0d5a821c51232b575000af8968d5a0
AlyasarJabbarli <alyasarmj@code.edu.az>
2026-04-04 02:45:15 +0200

    feat: update robots.txt rules to include disallow directive for /studio path, enhancing site crawling control

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,10 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      { userAgent: '*', allow: '/', disallow: ['/studio'] },
+    ],
     sitemap: 'https://alyasar.netlify.app/sitemap.xml',
   }
 }
